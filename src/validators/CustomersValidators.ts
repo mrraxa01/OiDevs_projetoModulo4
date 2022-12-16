aa183ae458731e27cce945f7e9d023bfcd2fe13c
Marcio Rodrigues <mrraxa01@gmail.com>
2022-12-16 20:29:33 -0300

    Added customer documentation by swagger

--- src/validators/CustomersValidators.ts
+++ src/validators/CustomersValidators.ts
@@ -1,7 +1,7 @@
 import { body, param } from "express-validator";
 
 export const customerIdValidation = [
-    param("id").notEmpty().withMessage("ID is required!").withMessage("ID is required!"),
+    param("id").notEmpty().isLength({min:10}).withMessage("Valid ID is required!"),
 ];
 
 export const createCustomerValidations = [
